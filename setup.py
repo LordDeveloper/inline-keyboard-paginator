4b0e036f1e4b981c0e7b99a3bb5134ca2b23ba27
lorddeveloper <losrkzpp@hi2.in>
2022-09-14 19:39:30 +0430

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='python-telegram-bot-pagination',
-    version='0.0.2',
-    packages=['telegram_bot_pagination'],
-    url='https://github.com/ksinn/python-telegram-bot-pagination',
+    name='inline-keyboard-paginator',
+    version='0.0.1',
+    packages=['inline_keyboard_paginator'],
+    url='https://github.com/LordDeveLoper/inline-keyboard-paginator',
     license='GPL2',
-    author='ksinn',
-    author_email='ksinnd@gmail.com',
-    description='Python inline keyboard pagination for Telegram Bot API',
+    author='LordDeveloper',
+    author_email='sadeghij87@gmail.com',
+    description='Python inline keyboard pagination MTProto libraries',
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords='telegram bot api pagination keyboard inline tools',
